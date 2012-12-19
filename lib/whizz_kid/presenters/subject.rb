a174b8cad580da0ca1ed8f2fa563cd8acdddf542
Ben Kitzelman <benkitzelman@gmail.com>
2012-12-19 17:23:17 +1100

    Changing button look

--- lib/whizz_kid/presenters/subject.rb
+++ lib/whizz_kid/presenters/subject.rb
@@ -16,19 +16,19 @@ module WhizzKid
           :':display' => {
             :':type'          => "application/vnd.playup.display.tile.solid+json",
             :title            => @subject.title,
-            :background_color => "0xAE2222",
+            :background_color => "0x5893CE",
             :background_image => [
               {
                 density: "low",
-                href: "http://s3-us-west-2.amazonaws.com/playup-tms/organization_4/images/1355284288.jpg"
+                href: "http://s3-us-west-2.amazonaws.com/playup-tms/organization_1/images/question_mark.png"
               },
               {
                 density: "high",
-                href: "http://s3-us-west-2.amazonaws.com/playup-tms/organization_4/images/1355284288.jpg"
+                href: "http://s3-us-west-2.amazonaws.com/playup-tms/organization_1/images/question_mark.png"
               },
               {
                 density: "medium",
-                href: "http://s3-us-west-2.amazonaws.com/playup-tms/organization_4/images/1355284288.jpg"
+                href: "http://s3-us-west-2.amazonaws.com/playup-tms/organization_1/images/question_mark.png"
               }
             ],
             :footer_title => "",
